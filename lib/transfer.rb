185bb5e3ddc57bc81c04da69a5b3277ee480c035
Michael Waide <mvwaide@gmail.com>
2020-07-21 14:43:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else @sender.balance < @amount
-           @status == "rejected"
+           @status = "rejected"
            return "Transaction rejected. Please check your account balance."
 
 
